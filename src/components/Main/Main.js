881faa1f1e50077f514bfb84ae91621386388200
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-02-04 12:23:10 -0400

    main js no 1

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+//add a state called users. 
+import {useEffect, useState} from 'react';
+function Main({domain})
+{    
+    return(
+        <>
+            <h1>User List</h1>    
+
+           
+        </>
+        
+    )
+}
+
+export default Main;
