fc3fda6857427a91e30f5e989aba83ce7acd0425
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-02-04 12:35:57 -0400

    e

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,21 @@
 //add a state called users. 
 import {useEffect, useState} from 'react';
+import M from 'materialize-css';
+import './Main.css';
 function Main({domain})
 {    
     return(
         <>
-            <h1>User List</h1>    
+        <form action="">
+        <h3>CV Form</h3><br/>
+        Your Name? <input type="text" name="name"/><br/><br/>
+        Education?<input type="text" name="education"/><br/><br/>
+        Awards: <input type="text" name="awards"/><br/><br/>
+        Work Experience: <input type="text" name="workExperience"/><br/><br/>
+        Skills: <input type="text" name="skills"/><br/><br/>
 
+        <input type="submit"/>
+    </form> 
            
         </>
         
@@ -13,3 +23,5 @@ function Main({domain})
 }
 
 export default Main;
+
+//A HTML Form that takes Name, Surname, Education, Awards, Work Experience and Skills as input 
