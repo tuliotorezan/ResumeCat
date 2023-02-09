c252608c1ce73046be7e73bac7499041267aac0c
Tulio Torezan <tuliotorezan@gmail.com>
2023-02-09 18:40:30 -0400

    work experience component done

--- src/Components/WorkExperience/WorkExperience.js
+++ src/Components/WorkExperience/WorkExperience.js
@@ -1,28 +1,30 @@
 import 'materialize-css/dist/css/materialize.min.css'
 
-function WorkExperience({data}) {
+function WorkExperience({ data }) {
     return (
         <div className='section row'>
             <div className='col left-align s6 offset-s3'>
                 <h6 className='sectionHead'>WORK EXPERIENCE</h6>
-                <div className='col left-align s12'>
-                    <div className='col s6 jobTitle'><h6>{data[0].position} | {data[0].name}</h6></div>
-                    <div className='col s6 jobDates right-align'><h6>{data[0].startDate} - {data[0].endDate}</h6></div>
-                </div>
+                {data.map(job =>
+                    <>
+                        <div className='col left-align s12'>
+                            <div className='col s6 jobTitle'><h6>{job.position} | {job.name}</h6></div>
+                            <div className='col s6 jobDates right-align'><h6>{job.startDate} - {job.endDate}</h6></div>
+                        </div>
 
-                <div className='col left-align s12'>
-                    <div className='col left-align s12 valign-wrapper'>
-                        <i className="tiny material-icons icon">open_in_new</i>
-                        <a>{data[0].url}</a>
-                    </div>
-                </div>
+                        <div className='col left-align s12'>
+                            <div className='col left-align s12 valign-wrapper'>
+                                <i className="tiny material-icons icon">open_in_new</i>
+                                <a>{job.url}</a>
+                            </div>
+                        </div>
 
-                <div className='col left-align s12'>
-                    <p className='col s12'>{data[0].summary}</p>
-                    <ul>
-                        {data[0].highlights.map(e=><li key={e}>{e}</li>)}
-                    </ul>
-                </div>
+                        <div className='col left-align s12'>
+                            <p className='col s12'>{job.summary}</p>
+                            {job.highlights.map(e =><div key={e} className="col skill-card grey lighten-2"><p className="card-content">{e}</p></div>)}
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     )
