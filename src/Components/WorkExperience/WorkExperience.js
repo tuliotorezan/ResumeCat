0dc95b44b517f74da6f2d02636110d45e0db432c
Tulio Torezan <tuliotorezan@gmail.com>
2023-02-09 18:26:43 -0400

    work experience component almost done

--- src/Components/WorkExperience/WorkExperience.js
+++ src/Components/WorkExperience/WorkExperience.js
@@ -4,26 +4,28 @@ function WorkExperience({data}) {
     return (
         <div className='section row'>
             <div className='col left-align s6 offset-s3'>
-                <h6 className='sectionHead'>Work Experience</h6>
+                <h6 className='sectionHead'>WORK EXPERIENCE</h6>
                 <div className='col left-align s12'>
+                    <div className='col s6 jobTitle'><h6>{data[0].position} | {data[0].name}</h6></div>
+                    <div className='col s6 jobDates right-align'><h6>{data[0].startDate} - {data[0].endDate}</h6></div>
                 </div>
 
-            </div>
+                <div className='col left-align s12'>
+                    <div className='col left-align s12 valign-wrapper'>
+                        <i className="tiny material-icons icon">open_in_new</i>
+                        <a>{data[0].url}</a>
+                    </div>
+                </div>
 
+                <div className='col left-align s12'>
+                    <p className='col s12'>{data[0].summary}</p>
+                    <ul>
+                        {data[0].highlights.map(e=><li key={e}>{e}</li>)}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default WorkExperience
-
-// "work": [{
-//     "name": "Company",
-//     "position": "President",
-//     "url": "https://company.com",
-//     "startDate": "2013-01-01",
-//     "endDate": "2014-01-01",
-//     "summary": "Description…",
-//     "highlights": [
-//       "Started the company"
-//     ]
-//   }],
