8197cd4f99ae499b428c0c5bd67c639d71dc1111
yeaat <w0471994@campus.nscc.ca>
2023-02-13 11:41:23 -0400

    dynamic phone

--- src/Components/Main/Mycomponents/InteractiveBlock.js
+++ src/Components/Main/Mycomponents/InteractiveBlock.js
@@ -3,6 +3,8 @@ import Intereducation from './Intereducation';
 export default InteractiveBlock;
 
 function InteractiveBlock(props) {
+
+    //Convert props to individual variables
     const toRender = props.type;
     
     switch(toRender) {
@@ -24,20 +26,37 @@ function InteractiveBlock(props) {
 
 }
 
-function BLK_Personal() {
+function BLK_Personal() {      
+    //Set variables for user requests for certain sections
+    const [phoneWanted, set_phoneWanted] = useState(0);
+
   return(
       <>
 <form>
+      <div className='basicBox'>
       <label>What is your name?</label><br/>
       <input type="text" name="name" placeholder="Purrito Cat"></input><br/>
+      </div>
       <label>Your title:</label>
       <input type="text" name="title" placeholder="Box Inspector"></input><br/>
       <label>Address Information:</label>
       <input type="text" name="address" placeholder="Cattown, New Meowia"></input><br/>
       <label>Email:</label>
       <input type="email" name="email" placeholder="purrito@meow.cat"></input><br/>
+      
+      {phoneWanted ? 
+      <>
       <label>Phone Number:</label><br/>
       <input type="tel" name="phoneNumber" placeholder="9025555555"></input><br/>
+      <button onClick={()=>set_phoneWanted(0)}>📞 Remove phone number</button>
+      <br/>
+      </>
+      : 
+      <>
+      <button onClick={()=>set_phoneWanted(1)}>📞 Add phone number</button>
+      <br/>
+      </>}
+
       <label>Personal Website:</label>
       <input type="url" name="personalWebsite"></input><br/>
       <label for="description">Description:</label><br/>  
